d3b0af3c260e7862abc8ead381fbbe43cd7e4cbe
GabrielNunes5 <gabrielnunesdev01@gmail.com>
2025-09-01 20:24:24 -0300

    feat: :sparkles: adiciona interface Animal e dados de animais fictícios

---
+++ src/app/models/animal.model.ts
@@ -0,0 +1,15 @@
+export interface Animal {
+    animalId: string;
+    animalName: string;
+    animalAge: string;
+    animalWeight: number;
+    animalSex: string;
+    animalColor?: string;
+    animalSpecies?: string;
+    animalVaccines?: string;
+    animalSize?: string;
+    animalNeutered?: boolean;
+    animalSpecialConditions?: string;
+    animalImageUrl: string;
+    animalAdopted: boolean;
+}
