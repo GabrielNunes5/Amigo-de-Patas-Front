27610ecc76b8264bb581587501d07aadc104f092
GabrielNunes5 <gabrielnunesdev01@gmail.com>
2025-09-05 13:41:13 -0300

    feat: :sparkles: adicionando endpoint animais

---
+++ src/app/pages/animais/animais.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-animais',
+  imports: [],
+  templateUrl: './animais.component.html',
+  styleUrl: './animais.component.css'
+})
+export class AnimaisComponent {
+
+}
