c1a4dbfad249f043ba6634c476d66164266d309c
GabrielNunes5 <gabrielnunesdev01@gmail.com>
2025-09-05 13:41:45 -0300

    feat: :sparkles: adicionando novas rotas dentro do app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,24 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ContatoComponent } from './pages/contato/contato.component';
+import { AnimaisComponent } from './pages/animais/animais.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
 
 export const routes: Routes = [
-    {path:"", component: HomeComponent}
+    {
+        path:"",
+        component: HomeComponent
+    },
+    {
+        path:"contato",
+        component: ContatoComponent
+    },
+    {
+        path:"animais", 
+        component: AnimaisComponent
+    },
+    {
+        path:"sobre",
+        component: SobreComponent
+    }
 ];
