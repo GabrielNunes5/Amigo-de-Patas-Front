329b19be4fc546eaea83a5ae5b48c6fdceb19117
GabrielNunes5 <gabrielnunesdev01@gmail.com>
2025-09-14 19:13:07 -0300

    refactor: :ambulance: ajustando para boas praticas de codigo angular

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,28 +1,40 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, signal } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
-import { Heart, Home, LucideAngularModule, Menu, PawPrint, Phone, Users, X, } from 'lucide-angular';
 import { CommonModule } from '@angular/common';
+import { LucideAngularModule, Heart, Home, Menu, PawPrint, Phone, Users, X } from 'lucide-angular';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, LucideAngularModule,CommonModule],
+  imports: [CommonModule, RouterLink, LucideAngularModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  isScrolled = false;
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    this.isScrolled = window.scrollY > 10;
-  }
-  mobileMenuOpen = false;  
+  isScrolled = signal(false);
+  mobileMenuOpen = signal(false);
+
   navigationItems = [
-    {title: "Início", url: "/", icon: Home},
-    {title: 'Animais', url: '/animais', icon: Heart },
-    {title: 'Sobre Nós', url: '/sobre', icon: Users },
-    {title: 'Contato', url: '/contato', icon: Phone }
-  ]
+    { title: 'Início', url: '/', icon: Home },
+    { title: 'Animais', url: '/animais', icon: Heart },
+    { title: 'Sobre Nós', url: '/sobre', icon: Users },
+    { title: 'Contato', url: '/contato', icon: Phone }
+  ];
+
   constructor(public router: Router) {}
+
+  @HostListener('window:scroll')
+  onWindowScroll() {
+    this.isScrolled.set(window.scrollY > 10);
+  }
+
+  toggleMobileMenu() {
+    this.mobileMenuOpen.update(open => !open);
+  }
+
+  closeMobileMenu() {
+    this.mobileMenuOpen.set(false);
+  }
+
   readonly PawPrint = PawPrint;
   readonly Menu = Menu;
   readonly X = X;
