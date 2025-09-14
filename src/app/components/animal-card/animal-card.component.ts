f1ce8a65e90564f9b4f1c318570e163f632df025
GabrielNunes5 <gabrielnunesdev01@gmail.com>
2025-09-14 18:44:24 -0300

    refactor: :ambulance: ajustando para boas praticas de codigo angular

--- src/app/components/animal-card/animal-card.component.ts
+++ src/app/components/animal-card/animal-card.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, computed, input, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Calendar, Heart, LucideAngularModule, Ruler, User } from 'lucide-angular';
 import { Animal } from '../../models/animal.model';
@@ -11,21 +11,36 @@ import { Animal } from '../../models/animal.model';
   styleUrl: './animal-card.component.css'
 })
 export class AnimalCardComponent {
-  @Input() animal!: Animal;
-
-  tipoColors: Record<string, string> = {
-    cachorro: 'bg-blue-100 text-blue-700 border-blue-200',
-    gato: 'bg-purple-100 text-purple-700 border-purple-200'
-  }
-
-  porteColors: Record<string, string> = {
-    pequeno: 'bg-green-100 text-green-700 border-green-200',
-    médio: 'bg-yellow-100 text-yellow-700 border-yellow-200',
-    grande: 'bg-orange-100 text-orange-700 border-orange-200'
-  }
-
   readonly Heart = Heart;
   readonly Calendar = Calendar;
   readonly User = User;
-  readonly Ruler = Ruler
+  readonly Ruler = Ruler;
+
+
+  readonly animal = input.required<Animal>();
+
+  private readonly tipoColors = signal<Record<string, string>>({
+    cachorro: 'bg-blue-100 text-blue-700 border-blue-200',
+    gato: 'bg-purple-100 text-purple-700 border-purple-200',
+  });
+
+  private readonly porteColors = signal<Record<string, string>>({
+    pequeno: 'bg-green-100 text-green-700 border-green-200',
+    médio: 'bg-yellow-100 text-yellow-700 border-yellow-200',
+    grande: 'bg-orange-100 text-orange-700 border-orange-200',
+  });
+
+  readonly speciesClass = computed(() => {
+    const species = this.animal()?.animalSpecies?.toLowerCase();
+    return species ? this.tipoColors()[species] || '' : '';
+  });
+
+  readonly sizeClass = computed(() => {
+    const size = this.animal()?.animalSize?.toLowerCase();
+    return size ? this.porteColors()[size] || '' : '';
+  });
+
+  readonly displayDescription = computed(() =>
+    this.animal()?.animalSpecialConditions || 'Sem observações especiais'
+  );
 }
