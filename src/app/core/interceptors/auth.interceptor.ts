ee85641b3394816dac1c20c10eda634a2c25bbbc
GabrielNunes5 <gabrielnunesdev01@gmail.com>
2025-10-07 17:15:23 -0300

    fix: :bug: Corrigindo Interceptor para fazer lougout automatico quando token invalido

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,18 +1,31 @@
-import { HttpInterceptorFn } from "@angular/common/http";
+import { HttpErrorResponse, HttpInterceptorFn } from "@angular/common/http";
+import { Router } from "@angular/router";
+import { catchError, throwError } from "rxjs";
+import { AuthService } from "../../service/auth/auth.service";
+import { inject } from "@angular/core";
 
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-    const accessToken = localStorage.getItem("accessToken");
+    const router = inject(Router);
+    const authService = inject(AuthService);
+    const token = localStorage.getItem('accessToken');
 
-    if (accessToken) {
-        const authReq = req.clone({
-            setHeaders: { 
-                Authorization: `Bearer ${accessToken}`,
-                'Content-Type': 'application/json'
-            }
-        });
-        return next(authReq);
-    }
+    const authReq = token
+        ? req.clone({
+            setHeaders: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+            },
+        })
+        : req;
 
-    return next(req);
-}
+    return next(authReq).pipe(
+        catchError((err: HttpErrorResponse) => {
+        if (err.status === 401) {
+            authService.logout();
+            router.navigate(['/auth/login']);
+        }
+        return throwError(() => err);
+        })
+    );
+};
