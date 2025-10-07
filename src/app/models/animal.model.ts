adecf9325e8ded13f5b2063439f3f68e15dd3ad9
GabrielNunes5 <gabrielnunesdev01@gmail.com>
2025-10-07 17:16:17 -0300

    perf: :zap: Retirando a necessidade de dados que ja estão vindo da API

--- src/app/models/animal.model.ts
+++ src/app/models/animal.model.ts
@@ -17,11 +17,6 @@ export interface Animal {
 export interface AdocaoFormData {
     animal_id: string;
     animal_name: string;
-    nome_interessado: string;
-    email: string;
-    telefone: string;
-    tipo_moradia: string;
-    tem_quintal: boolean;
     experiencia_animais: string;
     outros_animais: string;
     mensagem: string;
