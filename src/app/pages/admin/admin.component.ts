c4a7309e0128396d0222310c0ca4f8d25c1b74ea
GabrielNunes5 <gabrielnunesdev01@gmail.com>
2026-02-02 18:03:28 -0300

    feat: :sparkles: Adicionando componente AdminAdoptionForm e lógica para carregar formulários de adoção

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,26 +1,31 @@
 import { Component, signal, inject, effect, afterNextRender, DestroyRef, OnInit } from '@angular/core';
 import { AnimalService } from '../../service/animal/animal.service';
-import { Animal } from '../../models/animal.model';
+import { AdocaoFormData, Animal } from '../../models/animal.model';
 import { LucideAngularModule } from 'lucide-angular';
 import { AdminAnimaisComponent } from '../../components/admin-animais/admin-animais.component';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { AdminAdoptionFormComponent } from "../../components/admin-adoption-form/admin-adoption-form/admin-adoption-form.component";
+import { AdoptionFormService } from '../../service/adoptionForm/adoptionForm.service';
 
 @Component({
   selector: 'app-admin',
-  imports: [LucideAngularModule, AdminAnimaisComponent],
+  imports: [LucideAngularModule, AdminAnimaisComponent, AdminAdoptionFormComponent],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
 export class AdminComponent implements OnInit {
   private readonly animalService = inject(AnimalService);
+  private readonly adoptionFormService = inject(AdoptionFormService);
   private readonly destroyRef = inject(DestroyRef);
 
   loading = signal(false);
   saving = signal(false);
   animais = signal<Animal[]>([]);
+  adoptionForm = signal<AdocaoFormData[]>([]);
 
   ngOnInit(): void {
     this.loadAnimais();
+    this.loadAdoptionForm();
   }
 
   loadAnimais(): void {
@@ -88,6 +93,21 @@ export class AdminComponent implements OnInit {
           console.error('Erro ao atualizar animal', err);
         },
         complete: () => this.saving.set(false)
+      });
+  }
+
+  loadAdoptionForm(): void {
+    this.loading.set(true);
+
+    this.adoptionFormService.getAdoptionForms()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: list => this.adoptionForm.set(list),
+        error: err => {
+          console.error('Erro ao carregar formulários de adoção', err);
+          this.adoptionForm.set([]);
+        },
+        complete: () => this.loading.set(false),
       });
   }
 }
