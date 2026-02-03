81558c6f7773aaf231022b26de3c3df84b746c19
GabrielNunes5 <gabrielnunesdev01@gmail.com>
2026-02-03 20:03:11 -0300

    feat: :sparkles: Adicionando interface VoluntaryRequest para gerenciar dados de voluntários

---
+++ src/app/models/voluntary.model.ts
@@ -0,0 +1,15 @@
+export interface VoluntaryRequest {
+    voluntaryName: string;
+    voluntaryEmail: string;
+    voluntaryPhone: string;
+    voluntaryBirthDate: Date;
+    voluntaryOccupation: string;
+    voluntaryInterestArea: string;
+    voluntaryAvailability: string;
+    voluntaryExperience: string;
+    voluntaryMotivation: string;
+    hasTransportation: boolean;
+    hasRescueExperience: boolean;
+    canWorkOnWeekends: boolean;
+    voluntarySpecialSkills: string;
+    }
