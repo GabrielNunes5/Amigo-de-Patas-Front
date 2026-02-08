88505c00ff089f114f37ea75b6aff0a078d3aa2a
GabrielNunes5 <gabrielnunesdev01@gmail.com>
2026-02-08 18:07:56 -0300

    feat: :sparkles: Refatorando VoluntarioService para incluir injeção de dependência e cache de voluntários

--- src/app/service/voluntario/voluntario.service.ts
+++ src/app/service/voluntario/voluntario.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable, shareReplay } from 'rxjs';
+import { Voluntary } from '../../models/voluntary.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VoluntarioService {
-  private apiUrl = `${environment.apiUrl}voluntarios`;
+  private apiUrl = `${environment.apiUrl}voluntary`;
+  private http = inject(HttpClient);
+
+  private voluntaryCache$?: Observable<Voluntary[]>;
+
+  getVoluntaries(): Observable<Voluntary[]> {
+    if (!this.voluntaryCache$){
+      this.voluntaryCache$ = this.http.get<{ data: { content: Voluntary[] } }>(this.apiUrl).pipe(
+        map(response => response.data.content),
+        shareReplay({ bufferSize: 1, refCount: true })
+      );
+    }
+    return this.voluntaryCache$;
+  }
+
+  createVoluntary(
+    data: Partial<Voluntary>): Observable<Voluntary> {
+      return this.http.post<Voluntary>(this.apiUrl, data);
+    }
 }
