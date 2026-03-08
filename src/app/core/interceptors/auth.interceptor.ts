959bf86c91b30487ef61a4724e793d4336ea6efa
GabrielNunes5 <gabrielnunesdev01@gmail.com>
2026-03-08 15:22:49 -0300

    refactor: :recycle: Simplificar a lógica de autenticação no interceptor HTTP

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -4,38 +4,21 @@ import { catchError, throwError } from "rxjs";
 import { AuthService } from "../../service/auth/auth.service";
 import { inject } from "@angular/core";
 
-
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
     const router = inject(Router);
     const authService = inject(AuthService);
-    const token = localStorage.getItem('accessToken');
 
-    let authReq = req;
-
-    if (token) {
-        if (req.body instanceof FormData) {
-        authReq = req.clone({
-            setHeaders: {
-            Authorization: `Bearer ${token}`,
-            },
-        });
-        } else {
-        authReq = req.clone({
-            setHeaders: {
-            Authorization: `Bearer ${token}`,
-            'Content-Type': 'application/json',
-            },
-        });
-        }
-    }
+    const authReq = req.clone({
+        withCredentials: true,
+    });
 
     return next(authReq).pipe(
         catchError((err: HttpErrorResponse) => {
-        if (err.status === 401) {
-            authService.logout();
-            router.navigate(['/auth/login']);
-        }
-        return throwError(() => err);
+            if (err.status === 401) {
+                authService.logout();
+                router.navigate(['/auth/login']);
+            }
+            return throwError(() => err);
         })
     );
 };
