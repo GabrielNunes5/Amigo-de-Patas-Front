433af5cba685421e8fe344b4bf0431cf0b469f00
GabrielNunes5 <gabrielnunesdev01@gmail.com>
2026-03-11 18:58:11 -0300

    fix: :bug: adicionando onInit ao AppComponente para manter a chamada ao profile do usuario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { AuthService } from './service/auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,11 @@ import { FooterComponent } from "./components/footer/footer.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public auth = inject(AuthService);
   title = 'amigo-de-patas';
+
+  ngOnInit() {
+    this.auth.loadUserProfile();
+  }
 }
